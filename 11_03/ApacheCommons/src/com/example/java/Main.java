de61adff994a24c1b53b7950e6b1384796f8ffd6
pkumanowski <pkumanowski@outlook.com>
2017-12-12 16:17:57 +0100

    NewIO

--- 11_03/ApacheCommons/src/com/example/java/Main.java
+++ 11_03/ApacheCommons/src/com/example/java/Main.java
@@ -1,13 +1,22 @@
 package com.example.java;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        File sourceFile = new File("files/loremipsum.txt");
-        File targetFile = new File("files/target.txt");
+        File sourceFile = new File("C:\\Users\\pawel.kumanowski\\Documents\\newRepo\\ManagingFiles2\\11_03\\ApacheCommons\\files/loremipsum.txt");
+        File targetFile = new File("C:\\Users\\pawel.kumanowski\\Documents\\newRepo\\ManagingFiles2\\11_03\\ApacheCommons\\files/target.txt");
 
+        try {
+            FileUtils.copyFile(sourceFile, targetFile);
+            System.out.println("file copied!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
