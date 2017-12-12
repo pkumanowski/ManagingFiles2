5f20f2b851ab76d6e88bf68c63f0d1a74192fec8
pkumanowski <pkumanowski@outlook.com>
2017-12-12 14:54:43 +0100

    NewIO

--- 11_02/NewIO/src/com/example/java/Main.java
+++ 11_02/NewIO/src/com/example/java/Main.java
@@ -1,9 +1,24 @@
 package com.example.java;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Path sourceFile = Paths.get("C:\\Users\\pawel.kumanowski\\Documents\\newRepo\\ManagingFiles2\\11_02\\NewIO\\files\\", "loremipsum.txt");
+        Path targetFile = Paths.get("C:\\Users\\pawel.kumanowski\\Documents\\newRepo\\ManagingFiles2\\11_02\\NewIO\\files\\", "target.txt");
+
+        try {
+            Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File copied");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
